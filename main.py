de6b640c45d5774f6d479ba8c7c707d7de97bb6d
Eduardo Pinheiro <3dup1n@gmail.com>
2021-09-06 21:11:08 -0300

    fix: module fix

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from server import App
-from scrapper import scrape_google_play
+from services.scrapper import scrape_google_play
 
 app = App()
 
-@app.get("/{app_id}")
-async def root(app_id: str):
+@app.get('/')
+async def root():
+    return "hello scrappers"
+
+@app.get("/scrape/{app_id}")
+async def scrape(app_id: str):
     return scrape_google_play(app_id)
